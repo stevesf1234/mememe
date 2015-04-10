ae7ee2408eda39a4c7b6b81dbde79b434e9911e0
Stephen Fong <stevesf1234@gmail.com>
2015-04-09 17:10:39 -0700

    Committed by Udacity

---
+++ MemeMe/MemeMe/Meme.swift
@@ -0,0 +1,20 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Stephen Fong on 4/7/15.
+//  Copyright (c) 2015 Stephen Fong. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    
+    let topText: String
+    let bottomText: String
+    let image: UIImage
+    let memedImage: UIImage
+    
+}
+
