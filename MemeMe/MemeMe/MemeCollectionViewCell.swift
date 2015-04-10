12832cc657c8dc245799830b8885053c00ef4e33
Stephen Fong <stevesf1234@gmail.com>
2015-04-09 17:10:39 -0700

    Committed by Udacity

---
+++ MemeMe/MemeMe/MemeCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MemeCollectionViewCell.swift
+//  MemeMe
+//
+//  Created by Stephen Fong on 4/8/15.
+//  Copyright (c) 2015 Stephen Fong. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!    
+    
+}
